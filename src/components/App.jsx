3f4f6c7628df0d17640bee77189c00a431b28195
Comondear <elcomondear@gmail.com>
2023-01-16 13:52:26 -0700

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import React from "react";
+import { Form } from "./Form/Form";
+import { ContactsList } from './ContactsList/ContactsList'
+import { Filter } from "./Filter/Filter";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "redux/operations";
+import { selectIsLoading, selectError, selectContacts } from 'redux/selectors';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  const contacts = useSelector(selectContacts);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <>
+      <h1>Phonebook</h1>
+      <Form />
+      <h2>Contacts</h2>
+      <Filter />
+      {contacts.length > 0 && <ContactsList />}
+      {isLoading && !error && <b>Request in progress...</b>}
+    </>
+  )
+}
+
---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './contacts';
+import { filterReducer } from './filter';
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
